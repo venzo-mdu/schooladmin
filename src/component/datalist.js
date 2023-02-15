88ea25635276e4599153cdf33f313d479873312e
priyachandrasekar <priyankac@venzotechnologies.com>
2023-02-15 13:49:28 +0530

    update

--- src/component/datalist.js
+++ src/component/datalist.js
@@ -1,20 +1,39 @@
 import React from 'react'
 
-const datalist =(props) => 
-{
+const datalist = (props) => {
     console.log(props)
-  return (
-    <div>
-      {props.data.map (item => {
-        return <div>
-            <p>{item.name}</p>
-            <div>
-                
-            </div>
+    return (
+        <div>
+            {props.data.map(item => {
+                return <div>
+                    <p>{item.name}</p>
+                    <div>
+
+                        <table>
+                            {item.tabletitle.map(item => {
+                                return <tr>
+                                    <th>{item.name}</th>
+                                    <th>{item.id}</th>
+                                    <th>{item.class}</th>
+                                    <th>{item.subject}</th>
+                                </tr>
+                            })}
+                            {item.tablecont.map(item => {
+                                return <tr>
+                                    <td>{item.name}</td>
+                                    <td>{item.id}</td>
+                                    <td>{item.class}</td>
+                                    <td>{item.subject}</td>
+                                </tr>
+                            })}
+                        </table>
+
+
+                    </div>
+                </div>
+            })}
         </div>
-      })}
-    </div>
-  )
+    )
 }
 
 export default datalist
--- src/layout/route.js
+++ src/layout/route.js
@@ -3,13 +3,14 @@ import {Route,Routes} from 'react-router-dom'
 import Datalist from '../component/datalist'
 import Coursedetail from '../component/coursedetail'
 import Studentdata from '../content/studentData.json'
+import Coursedata from '../content/courseData.json'
 
 function route() {
   return (
     <div>
         <Routes>
         <Route path='/studentlist' element={<Datalist data={Studentdata}/>} /> 
-        <Route path='/coursedetail' element={<Coursedetail/>} /> 
+        <Route path='/coursedetail' element={<Datalist data={Coursedata}/>} /> 
       </Routes>
     </div>
   )
